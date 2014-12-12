2b8b96fa0dfbe81fd1c4f5aae1fff5fe97cc9c37
suyuwen1 <suyuwen1@126.com>
2014-12-12 17:57:02 +0800

    2014.12.12

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>目录</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link href="" rel="stylesheet">
+</head>
+<body>
+	<?php
+    	/**
+    	* 
+    	*/
+    	class Rdir
+    	{
+    		protected $p;
+    		protected $s;
+    		protected $dirs;
+    		
+    		function __construct($a,$b='default')
+    		{
+    			$this->p=$a;
+    			$this->s=$b;
+    		}
+    		function eachdir()
+    		{
+    			$dirs=array();
+    			//$files=array();
+    			$d=opendir($this->p);
+    			while ($f=readdir($d)) {
+    			//echo $f.'<br>';
+    				$this->p=$this->p.'/'.$f;
+    				if (is_dir($this->p)) {
+    					$dirs['d'][]=$f;
+    				}else{
+    					$dirs['f'][]=$f;
+    					echo $this->p;
+    					$dirs['t'][]=filectime($this->p);
+    				}
+    			}
+    			closedir($d);
+    		}
+    		function filesort()
+    		{
+    			array_multisort($this->dirs['t'],SORT_DESC,SORT_NUMERIC,$this->dirs['f']);
+    		}
+    		function data()
+    		{
+    			if ($this->s=='desc') {
+    				$this->filesort();
+    			}else{
+    				$this->eachdir();
+    			}
+    			return $this->dirs;
+    		}
+    	}
+    	$v=new Rdir(dirname(dirname(__FILE__)));
+    	var_dump($v->data());
+    	?>
+    </body>
+    </html>
